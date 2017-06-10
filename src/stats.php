4ed9545ac317ee8b7fed833e065c0dc4b5911ac2
Μιλινα <mgiallia@gmail.com>
2017-06-11 02:44:05 +0300

    pie chart st stats.php

--- src/stats.php
+++ src/stats.php
@@ -1,4 +1,80 @@
 <?php
 include_once 'utilities/connectWithDB.php';
+$query1 ="SELECT thesis_state.description,(plithos_per_state.plithos / COUNT(thesis.teacher_id)) * 100 as pososto FROM plithos_per_state,thesis,thesis_state where thesis_state.id=plithos_per_state.state GROUP by plithos_per_state.state";
+
+$result1 = mysqli_query($link, $query1);
+$rows1 = array();
+while ($query1 = mysqli_fetch_array($result1)) {
+//    $f_name = $query['f_name'];
+//    $l_name = $query['l_name'];
+    $count = $query1['pososto'];
+    $name = $query1['description'];
+    // input data into data structure
+    // typecast count as integer so it doesn't get interpreted as a string
+    $rows1[] = array($name, (int)$count);
+}
+$data = json_encode($rows1);
 
 ?>
+<html>
+
+<head>
+
+    <?php
+    include_once "page_parts/head.php";
+    ?>
+
+    <!--Load the AJAX API-->
+    <script type="text/javascript" src="https://www.google.com/jsapi"></script>
+    <script type="text/javascript">
+
+        // Load the Visualization API and the piechart package.
+        google.load('visualization', '1.0', {'packages': ['corechart']});
+
+        // Set a callback to run when the Google Visualization API is loaded.
+        google.setOnLoadCallback(piechart);
+
+        // Callback that creates and populates a data table,
+        // instantiates the pie chart, passes in the data and
+        // draws it.
+        function piechart() {
+
+            // Create the data table.
+            var data = new google.visualization.DataTable();
+            data.addColumn('string', 'description');
+            data.addColumn('number', 'plithos');
+            data.addRows(<?php echo $data; ?>);
+
+
+            // Set chart options
+            var options = {
+                'title': 'Ποσοστο διπλωματικων ανα ειδος', 'width': 800,
+                'height': 400
+            };
+
+            // Instantiate and draw our chart, passing in some options.
+            var chart = new google.visualization.PieChart(document.getElementById('chart'));
+            chart.draw(data, options);
+        }
+
+    </script>
+
+
+</head>
+<body class="conatainer">
+<?php
+include_once "page_parts/header.php";
+?>
+
+<?php
+include_once "page_parts/login_checker.php";
+?>
+
+<div class="page_content">
+    <!--Div that will hold the pie chart-->
+    <div id="chart" align="center"></div>
+
+</div>
+</body>
+
+</html>
